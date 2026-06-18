05213304a0fc5a7ff017d872ca46d17fbc4ef43a
Grace Elizabeth Jose <129204450+Grace1903@users.noreply.github.com>
2026-06-18 23:50:03 +0530

    Solved Array Q7 Max and Min

--- Arrays/q007_max_min.py
+++ Arrays/q007_max_min.py
@@ -1,6 +1,11 @@
-def maxno():
+def maxmin():
     arr=list(map(int,input().split()))
-    max=arr[0]
+    maxi=arr[0]
+    mini=arr[0]
     for x in arr:
-        if x>max:
-            max=x
+        if x>maxi:
+            maxi=x
+        if x<mini:
+            mini=x
+    print(maxi,mini)
+maxmin()
